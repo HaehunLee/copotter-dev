7c3c32d428ba78fda42d5206b507a5517d515b62
Copotter <allman956@naver.com>
2022-02-01 17:45:47 +0900

    Init: Boiler Plate next.js with typescript and styled-components

---
+++ styles/styled.d.ts
@@ -0,0 +1,12 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface CustomTheme {
+    default: {
+      textColor: string;
+    };
+    dark: {
+      textColor: string;
+    };
+  }
+}
---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+import { CustomTheme } from "styled-components";
+
+export const theme: CustomTheme = {
+  default: {
+    textColor: "#242424",
+  },
+  dark: {
+    textColor: "#ffffff",
+  },
+};
+
+const customMediaQuery = (maxWidth: number): string =>
+  `@media (max-width: ${maxWidth}px)`;
+
+export const media = {
+  custom: customMediaQuery,
+  pc: "@media (max-width: 1440px)",
+  tablet: "@media (max-width: 768px)",
+  mobile: "@media (max-width: 576px)",
+};
